c1c8a178f404d01068fb531faa713a34183f1ccc
jathu1994 <jathu10061994@gmail.com>
2021-05-31 20:39:15 +0530

    number reverser

--- Number_Reverser/Number_Reverser.js
+++ Number_Reverser/Number_Reverser.js
@@ -1,8 +1,12 @@
 function reversNumber(number) {
     let reversedNumber = 0;
     while(number > 0)
+
     {
+        //modulus by 10 will give same last digit
+        //multiply the result by 10 will make position correct for next addition
         reversedNumber = reversedNumber * 10 + number % 10;
+        //division and floring will result in removing the last degit
         number = Math.floor(number / 10);
     }
     return reversedNumber;
