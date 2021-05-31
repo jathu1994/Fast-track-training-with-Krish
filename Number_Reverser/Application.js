75315de809156dab2f40d3e2cf4cc136c063c5ea
jathu1994 <jathu10061994@gmail.com>
2021-05-31 20:34:51 +0530

    number reverser

---
+++ Number_Reverser/Application.js
@@ -0,0 +1,3 @@
+var {reversNumber}  = require('./Number_Reverser');
+
+console.log(reversNumber(1265));
---
+++ Number_Reverser/Number_Reverser.js
@@ -0,0 +1,13 @@
+function reversNumber(number) {
+    let reversedNumber = 0;
+    while(number > 0)
+    {
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number = Math.floor(number / 10);
+    }
+    return reversedNumber;
+}
+
+module.exports={
+    reversNumber
+}
